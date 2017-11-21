2340271936461fe1db1c8ae688d9e343caaefdf7
iiffd <sml.hng@gmail.com>
2017-11-20 16:36:01 -0800

    Reverse linked list

--- interview-practice/linked-lists/reverseNodesInKGroups.py
+++ interview-practice/linked-lists/reverseNodesInKGroups.py
@@ -6,41 +6,45 @@
 
 
 def reverseNodesInKGroups(l, k):
+    # If k is one, then just return list
+    if k == 1:
+        return l
+
     prev, head, i = None, None, 0
-    k_start, k_alt = None, None
+    k1, k2 = None, None
 
     while l:
         i += 1
-        print head
-        if i == k:
-            head = prev = l
-            l = l.next
-            continue
 
-        if l.next == None:
-            print head
-            if i / k  % 2 == 0:
-                k_start.next = k_alt
+        # Seperate into groups of k
+        if i == k:
+            head = l
+        elif i % k == 0:
+            if i / 3 % 2 == 0:
+                k2.next = l
             else:
-                k_alt.next = k_start
+                k1.next = l
+
+        # Check if value should hold alternating value
+        if i % k == 1:
+            if (i - 1) / k % 2 == 1:
+                k2 = l
+            else:
+                k1 = l
+
+        # Check if next is None
+        if l.next == None:
+            if (i - (i % k)) / k % 2 == 1:
+                print k1.value, k2.value
+            else:
+                #k2.next = k1
+                print 'k2', k2.value, 'k1', k1.value
             return head
 
-        if i % k == 0:
-            if i / k > 1:
-                if i / k  % 2 == 0:
-                    k_start.next = l
-                else:
-                    k_alt.next = l
-
-
-        if i % k == 1:
-            if (i - 1) / k % 2 == 0:
-                k_start = l
-            else:
-                k_alt = l
-            l = l.next
-
         next = l.next
-        l.next = prev
+        if i % k == 1:
+            l.next == None
+        else:
+            l.next = prev
         prev = l
         l = next
