26557e8459f43977b118b9033865c7ed2b21d173
iiffd <sml.hng@gmail.com>
2018-05-21 19:25:59 -0700

    Fix max heap implementation

--- toy-problems/maxHeap.js
+++ toy-problems/maxHeap.js
@@ -1,13 +1,64 @@
-const heapify = (arr, i) => {
-  const leftChild = i * 2 + 1;
-  const rightChild = i * 2 + 2;
+class Heap {
+  constructor() {
+    this.heap = [];
+  }
+  pop() {
+    const first = this.heap.pop();
+    const max = this.heap[0];
+    this.heap[0] = first;
+    this.sinkDown();
+    return max;
+  }
+  push(elem) {
+    this.heap.push(elem);
+    this.bubbleUp(this.heap.length - 1);
+    return this.heap;
+  }
+  bubbleUp(i) {
+    if (i > 0) {
+      const parentI = Math.floor(i + 1 / 2) - 1;
+      if (this.heap[parentI] < this.heap[i]) {
+        [this.heap[parentI], this.heap[i]] = [this.heap[i], this.heap[parentI]]
+        this.bubbleUp(parentI);
+      }
+    }
+  }
+  sinkDown(i = 0) {
+    if (i < this.heap.length) {
+      const leftChild = i * 2 + 1;
+      const rightChild = i * 2 + 2;
+      if (this.heap[leftChild] < this.heap[rightChild]) {
+        if (this.heap[rightChild] > this.heap[i]) {
+          [this.heap[i], this.heap[rightChild]] = [this.heap[rightChild], this.heap[i]]
+          this.sinkDown(rightChild);
+        }
+      } else {
+        if (this.heap[leftChild] > this.heap[i]) {
+          [this.heap[i], this.heap[leftChild]] = [this.heap[leftChild], this.heap[i]]
+          this.sinkDown(leftChild);
+        }
+      }
+    }
+  }
+  check() {
+    console.log(this.heap);
+  }
 
-  if (arr[i] < arr[leftChild]) [arr[i], arr[leftChild]] = [arr[leftChild], arr[i]];
-  if (arr[i] < arr[rightChild]) [arr[i], arr[rightChild]] = [arr[rightChild], arr[i]];
 
-  if (arr.length > leftChild) heapify(arr, leftChild);
-  if (arr.length > leftChild) heapify(arr, rightChild);
-  return arr;
 }
 
-heapify([4, 10, 3, 5, 1], i = 0);
+
+
+const test = new Heap([4, 2, 6, 8, 7, 1]);
+for (const elem of [4, 2, 6, 8, 7, 1]) {
+  test.push(elem);
+}
+test.check();
+test.pop();
+test.check();
+test.pop();
+test.check();
+test.pop();
+test.check();
+test.pop();
+test.check();
